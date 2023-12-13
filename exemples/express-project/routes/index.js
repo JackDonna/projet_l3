7673a4a647b6fd74344d656c329dc0641ed516ec
Jok3Noname <63104855+IK-NoName@users.noreply.github.com>
2023-12-13 13:14:25 +0100

    sous dossier en trop

---
+++ exemples/express-project/routes/index.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var router = express.Router();
+var escapeHtml = require('escape-html')
+var session = require('express-session')
+
+function isAuthenticated (req, res, next) {
+  if (req.session.user) next()
+  else next('route')
+}
+
+
+
+router.get('/', isAuthenticated, function (req, res) {
+  // this is only called when there is an authentication user due to isAuthenticated
+  res.send('hello, ' + escapeHtml(req.session.user) + '!' +
+      ' <a href="/logout">Logout</a>')
+})
+
+router.get('/', function (req, res) {
+  res.send('<form action="/login" method="post">' +
+      'Username: <input name="user"><br>' +
+      'Password: <input name="pass" type="password"><br>' +
+      '<input type="submit" text="Login"></form>')
+})
+
+router.post('/login', express.urlencoded({ extended: false }), function (req, res) {
+  // login logic to validate req.body.user and req.body.pass
+  // would be implemented here. for this example any combo works
+
+  // regenerate the session, which is good practice to help
+  // guard against forms of session fixation
+  req.session.regenerate(function (err) {
+    if (err) next(err)
+
+    // store user information in session, typically a user id
+    req.session.user = req.body.user
+
+    // save the session before redirection to ensure page
+    // load does not happen before session is saved
+    req.session.save(function (err) {
+      if (err) return next(err)
+      res.redirect('/')
+    })
+  })
+})
+
+
+router.get('/logout', function (req, res, next) {
+  // logout logic
+
+  // clear the user from the session object and save.
+  // this will ensure that re-using the old session id
+  // does not have a logged in user
+  req.session.user = null
+  req.session.save(function (err) {
+    if (err) next(err)
+
+    // regenerate the session, which is good practice to help
+    // guard against forms of session fixation
+    req.session.regenerate(function (err) {
+      if (err) next(err)
+      res.redirect('/')
+    })
+  })
+})
+module.exports = router;
