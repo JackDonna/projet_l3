91d7dfdfc0de6339d77f40e9258085092c607de0
AyRron <cyrilpitar@orange.fr>
2023-12-20 15:36:54 +0100

    Ajouts des Routes

---
+++ exemples/express-project/routes/neo.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET users listing. */
+router.get("/test", (req, res) => {
+    res.send("test de NEO4J");
+})
+module.exports = router;
