9d1a5a2eba92483f4af57f1597c39ec9138e289f
Jok3Noname <63104855+IK-NoName@users.noreply.github.com>
2024-01-11 11:16:04 +0100

    certificat

--- exemples/express-project/public/javascripts/calendar.js
+++ exemples/express-project/public/javascripts/calendar.js
@@ -41,8 +41,8 @@ window.addEventListener('resize', () => {
 const scanner = new Html5QrcodeScanner('reader', {
     // Scanner will be initialized in DOM inside element with id of 'reader'
     qrbox: {
-        width: 250,
-        height: 250,
+        width: 300,
+        height: 300,
     },  // Sets dimensions of scanning box (set relative to reader element width)
     fps: 60, // Frames per second to attempt a scan
 });
