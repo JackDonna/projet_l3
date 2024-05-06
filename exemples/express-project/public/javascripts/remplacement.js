1e75a98418618d0a235d46ec955f5845690408c3
mathisfeltrin <104620255+mathisfeltrin@users.noreply.github.com>
2024-05-06 18:33:36 +0200

    add fichier

--- exemples/express-project/public/javascripts/remplacement.js
+++ exemples/express-project/public/javascripts/remplacement.js
@@ -7,6 +7,19 @@ let remplacement = {
         console.log("test")
         this.data.forEach((evenement) => {
             console.log("evenement : ", evenement);
+            let div = document.createElement("div");
+            div.classList.add("box_absence");
+
+            div.innerHTML = `
+                    <p><span class="cle">Date : </span>${new Date(evenement.date).toLocaleDateString()}</p>
+                    <p><span class="cle">Heure de début : </span>${evenement.start.slice(0, -3).replace(":", "h")}</p>
+                    <p><span class="cle">Heure de fin : </span>${evenement.end.slice(0, -3).replace(":", "h")}</p>
+                    <p><span class="cle">Professeur : </span>${evenement.nom} ${evenement.prenom}</p>
+                    <p><span class="cle">Matière : </span>${truncateString(evenement.libelle_court, 33)}</p>
+                    <p class="classe_${evenement.id_abs}" ><span class="cle 
+                    }">Classe : </span>${evenement.classe}</p>
+                    <p><span class="cle">Motif : </span>${evenement.motif}</p>
+                `;
         });
     },
 
