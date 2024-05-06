20c1e5d5023b78f75ed41e64f4b0257d9c06257c
mathisfeltrin <104620255+mathisfeltrin@users.noreply.github.com>
2024-05-06 11:25:58 +0200

    sw de tes morts

--- exemples/express-project/public/javascripts/service_worker.js
+++ exemples/express-project/public/javascripts/service_worker.js
@@ -1,4 +1,30 @@
+const PREFIX = 'V1';
+
+self.addEventListener('install', (event) => {
+    console.log(`${PREFIX} install service worker`);
+})
+
+self.addEventListener('activate', (event) => {
+    console.log(`${PREFIX} Active service worker`);
+})
+
 self.addEventListener("fetch", (event) => {
-    console.log(`Fetching : ${event.request.url}, Mode : ${event.request.mode}`);
+    console.log(`${PREFIX} Fetching : ${event.request.url}, Mode : ${event.request.mode}`);
     console.log("test")
+    if(event.request.mode === 'navigate'){
+        event.respondWith((async () => {
+            try{
+                const preloadResponse = await event.preloadResponse();
+                if (preloadResponse){
+                    return preloadResponse
+                }
+
+                return await fetch(event.request);
+            } catch(e){
+                return new Response("Boujour les gens");
+            }
+
+
+        })())
+    }
 })
